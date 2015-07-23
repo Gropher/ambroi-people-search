0de637b892351314ff26b22f85e80b7fc1606985
gropher <grophen@gmail.com>
2015-07-23 13:50:58 +0300

    Relext thread pool added

--- app.rb
+++ app.rb
@@ -8,20 +8,38 @@ require 'sanitize'
 
 configure do
   set :redis, Redis.new(host: '127.0.0.1', port: '6379', db: 1)
-  (1..3).each do |i|
+  (1..10).each do |i|
     Thread.new do
       redis = Redis.new(host: '127.0.0.1', port: '6379', db: 1)
-      log "Thread ##{i} started" 
+      log "Search Thread ##{i} started" 
       loop do
         query = redis.brpop('queries').last
         begin
-          log "Thread ##{i} processing query: #{query}"
+          log "Search Thread ##{i} processing query: #{query}"
           parsed_query = JSON.parse query
           cognitive_search parsed_query['query'], redis, parsed_query['token']
-          log "Thread ##{i} finished query: #{query}"
+          log "Search Thread ##{i} finished query: #{query}"
         rescue => e
-          log "Thread ##{i} can't process query #{query}: #{$!}"
-          puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+          log "Search Thread ##{i} can't process query #{query}: #{$!}"
+          puts "Search Thread ##{i} backtrace:\n\t#{e.backtrace.join("\n\t")}"
+        end
+      end
+    end
+  end
+  (1..8).each do |i|
+    Thread.new do
+      redis = Redis.new(host: '127.0.0.1', port: '6379', db: 1)
+      log "RelExt Thread ##{i} started" 
+      loop do
+        url = redis.brpop('relext_requests').last
+        begin
+          log "RelExt Thread ##{i} processing url: #{url}"
+          relext = get_relext url, redis
+          redis.publish('relext', {url: url, relext: relext}.to_json)
+          log "RelExt Thread ##{i} finished url: #{url}"
+        rescue => e
+          log "RelExt Thread ##{i} can't process query #{url}: #{$!}"
+          puts "RelExt Thread ##{i} backtrace:\n\t#{e.backtrace.join("\n\t")}"
         end
       end
     end
@@ -33,8 +51,9 @@ get '/' do
 end
 
 post '/search' do
-  if params[:q].present?
-    redis.lpush 'queries', { query: params[:q], token: request.cookies['facebook_access_token'] }.to_json
+  query = normalize_query params[:q]
+  if query.present?
+    redis.lpush 'queries', { query: query, token: request.cookies['facebook_access_token'] }.to_json
     { data: 'success' }.to_json    
   else
     { error: 'blank_query' }.to_json
@@ -42,9 +61,10 @@ post '/search' do
 end
 
 get '/search_results' do
-  if params[:q].present?
-    md5 = "results_#{Digest::MD5.hexdigest params[:q]}"
-    results = JSON.parse(redis.get md5) rescue {'status' => 'in_progress', 'results' => {}, 'progress' => 0}
+  query = normalize_query params[:q]
+  if query.present?
+    md5 = "results_#{Digest::MD5.hexdigest query}"
+    results = JSON.parse(redis.get md5) rescue empty_results
     results['results'] = results['results'].map do |name, links|
       { name: name, links: links }
     end.compact.to_a.sort {|a,b| b[:links].count <=> a[:links].count } 
@@ -57,26 +77,34 @@ end
 def cognitive_search query, redis, token
   if query
     md5 = "results_#{Digest::MD5.hexdigest query}"
-    results = JSON.parse(redis.get md5) rescue {'status' => 'in_progress', 'results' => {}, 'progress' => 0}
+    results = JSON.parse(redis.get md5) rescue empty_results
     unless results['status'] == 'finished'
-      results = {'status' => 'in_progress', 'results' => {}, 'progress' => 0}
+      results = empty_results
       yandex = Hash.from_xml RestClient.get("https://yandex.com/search/xml?user=grophen&key=03.43282533:5e955fb84f7bf3dddd1ab1b14cc6eaa9&query=#{ERB::Util.url_encode query}&l10n=en&sortby=rlv&filter=moderate&groupby=attr%3D%22%22.mode%3Dflat.groups-on-page%3D100.docs-in-group%3D1")
-      yandex['yandexsearch']['response']['results']['grouping']['group'].each do |doc|
-        results['progress'] = results['progress'].to_i + 1
-        url = doc['doc']['url']
-        log "Processing URL: #{url}"
-        relext = get_relext url, redis
-        if relext
-          names = relext['doc']['mentions']['mention'].select {|m| m['role'] == 'PERSON' and m['mtype'] = 'NAM' and m['text'] =~ /\A([A-Z][a-z]+\s?){2,4}\Z/ }.map {|m| m['text'] } rescue []
-          names.each do |name|
-            checked_name = check_name!(name, redis, token)
-            if checked_name and checked_name != 'false'
-              results['results'][checked_name] ||= []
-              results['results'][checked_name] << url
-              results['results'][checked_name] = results['results'][checked_name].uniq
+      urls = yandex['yandexsearch']['response']['results']['grouping']['group'].map {|doc| doc['doc']['url'] }
+      urls.each do |url| 
+        redis.multi do |r|
+         r.lpush 'relext_requests', url
+        end
+      end
+      redis.subscribe('relext') do |on|
+        on.message do |channel, msg|
+          message = JSON.parse msg
+          url = message['url']
+          relext = message['relext']
+          results['progress'] = results['progress'].to_i + 1
+          if relext
+            names = relext['doc']['mentions']['mention'].select {|m| m['role'] == 'PERSON' and m['mtype'] = 'NAM' and m['text'] =~ /\A([A-Z][a-z]+\s?){2,4}\Z/ }.map {|m| m['text'] } rescue []
+            names.each do |name|
+              checked_name = check_name!(name, redis, token)
+              if checked_name and checked_name != 'false'
+                results['results'][checked_name] ||= []
+                results['results'][checked_name] << url
+                results['results'][checked_name] = results['results'][checked_name].uniq
+              end
             end
+            redis.setex(md5, 24*3600, results.to_json)
           end
-          redis.setex(md5, 24*3600, results.to_json)
         end
       end
       results['status'] = 'finished'
@@ -115,10 +143,10 @@ def check_name! name, redis, token
       end
     end
     if fb_names.present? and fb_names.map {|fb_name| levenshtein_distance checked_name, fb_name['name'] }.min < 2
-      redis.setex(md5, 7*24*3600, checked_name)
+      redis.setex(md5, 30*24*3600, checked_name)
       checked_name
     else
-      redis.setex(md5, 7*24*3600, false)
+      redis.setex(md5, 30*24*3600, false)
       false
     end
   end
@@ -144,16 +172,24 @@ def get_relext url, redis
     if content_type.to_s =~ /text\/html/
       text = RestClient.get(url) rescue nil
       text = CGI.unescapeHTML(Sanitize.fragment(text.to_s, remove_contents: [:link, :style, :script]).squish) rescue nil
-      relext = RestClient.post("http://ambroi.eu-gb.mybluemix.net/say/relext", text: text[0..15000]) rescue nil
-      redis.setex(md5, 7*24*3600, relext) if relext.present?
+      relext = RestClient.post("http://ambroi.eu-gb.mybluemix.net/say/relext", text: text[0..10000]) rescue nil
+      redis.setex(md5, 30*24*3600, relext) if relext.present?
     end
   end
   JSON.parse(relext) rescue nil
 end
 
+def normalize_query query
+  query.to_s.strip.downcase.squish
+end
+
 def log msg
-  puts "#{Time.now} === #{msg}"
+  puts "#{Time.now} #{'='*20} #{msg}"
   STDOUT.flush
+end
+
+def empty_results
+  { 'status' => 'in_progress', 'results' => {}, 'progress' => 0 }
 end
 
 def redis
